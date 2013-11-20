ddf795bce99f19ffd05783af4ec9dcbc7e182a27
shonenada <shonenada@gmail.com>
2013-11-21 00:39:43 +0800

    Added comments and some methods.

--- flask_rbac/__init__.py
+++ flask_rbac/__init__.py
@@ -11,10 +11,17 @@ class RBACRoleMixin(object):
     This mixin class provides implementations for the methods of Role model
     needed by Flask-RBAC.
     '''
+    @staticmethod
+    def get_roles(self):
+        '''A static method return a list of all roles'''
+        return self.query.all()
+
     def get_name(self):
+        '''Return the name of this role'''
         return self.name
 
     def get_parents(self):
+        '''Return parents of this role'''
         return self.parents
 
 
@@ -24,8 +31,21 @@ class RBACUserMixin(object):
     needed by Flask-RBAC.
     '''
     def get_roles(self):
+        '''Return roles of this user'''
         return self.roles
 
+
+class AccessControl(object):
+    '''
+    This class record data for access controling.
+    '''
+    def __init__(self):
+        self._resource = {}
+        self._allowed = {}
+        self._denied = {}
+
+    def allow(self, role, resource, method, assertion=None):
+        pass
 
 class _RBACState(object):
     '''Records configuration for Flask-RBAC'''
@@ -35,11 +55,25 @@ class _RBACState(object):
 
 
 class RBAC(object):
-    '''
-    '''
+    '''This class implements role-base access control module in flask.
 
+    There are two way to initialize Flask-RBAC:
+
+        app = Flask(__name__)
+        rbac = RBAC(app)
+
+    or
+
+        rbac = RBAC
+        def create_app():
+            app = Flask(__name__)
+            rbac.init_app(app)
+            return app
+
+    Difference between two ways see:
+    https://github.com/mitsuhiko/flask-sqlalchemy/blob/master/flask_sqlalchemy/__init__.py#L592
+    '''
     def __init__(self, app=None, **kwargs):
-        
         self._role_model = kwargs.get('role_model', None)
         self._user_model = kwargs.get('user_model', None)
         self._user_loader = kwargs.get('user_loader', None)
@@ -50,6 +84,12 @@ class RBAC(object):
             self.app = None
 
     def init_app(self, app):
+        '''
+        Initialize application in Flask-RBAC.
+
+        Adds (RBAC, app) to flask extensions.
+        Adds hook to authenticate permission before request.
+        '''
         self.app = app
 
         if not hasattr(app, 'extensions'):
@@ -59,21 +99,35 @@ class RBAC(object):
         app.before_request(self._authenticate)
 
     def set_role_model(model):
-        if not ('get_name' in dir(model) and 'get_parents' in dir(model)):
-            raise NotImplementedError(
-                "%s didn't implement 'get_name' or 'get_parents' methods!" %
-                model.__class__)
+        '''Set custom model of Role.'''
+        needed_methods = ['get_roles', 'get_name', 'get_parents']
+        for method in needed_methods:
+            if not method in dir(model):
+                raise NotImplementedError("%s didn't implement %s method!" %
+                                          model.__class__, method)
         self._role_model = model
 
     def set_user_model(model):
-        if not 'get_roles' in dir(model):
-            raise NotImplementedError(
-                "%s didn't implement 'get_roles' method!" % model.__class__)
+        '''Set custom model of User.'''
+        needed_methods = ['get_roles']
+        for method in needed_methods:
+            if not method in dir(model):
+                raise NotImplementedError("%s didn't implement %s method!" %
+                                          model.__class__, method)
         self._user_model = model
 
     def set_user_loader(loader):
+        '''Set user loader, which is used to load current user'''
         self._user_loader = loader
 
     def _authenticate(self):
         '''Authenticate permission'''
-        pass
+        assert self.app, "Please initialize your application into Flask-RBAC."
+        assert self._role_model, "Please set role model before authenticate."
+        assert self._user_model, "Please set user model before authenticate."
+        assert self._user_loader, "Please set user loader before authenticate."
+        
+        current_user = self._user_loader()
+        assert (type(current_user) == self._user_model,
+                "%s is not an instance of %s" %
+                (current_user, self._user_model.__class__))
