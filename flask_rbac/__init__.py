a8dff0de248e52ccb4370e14a297fbd856188abc
shonenada <shonenada@gmail.com>
2013-11-21 00:18:57 +0800

    Added two mixin class

--- flask_rbac/__init__.py
+++ flask_rbac/__init__.py
@@ -6,3 +6,22 @@
     Adds Role-based Access Control module to application.
 
 """
+class RBACRoleMixin(object):
+    '''
+    This mixin class provides implementations for the methods of Role model
+    needed by Flask-RBAC.
+    '''
+    def get_name(self):
+        return self.name
+
+    def get_parents(self):
+        return self.parents
+
+
+class RBACUserMixin(object):
+    '''
+    This mixin class provides implementations for the methods of User model
+    needed by Flask-RBAC.
+    '''
+    def get_roles(self):
+        return self.roles
