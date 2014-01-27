0c87a36cb39e699646ec2c45e2e0b41a8e49b17b
shonenada <shonenada@gmail.com>
2014-01-27 17:45:48 +0800

    add test case

--- test_rbac.py
+++ test_rbac.py
@@ -14,19 +14,19 @@ class User(UserMixin):
         return '<User %s>' % self.roles
 
 everyone = Role('everyone')
-local_user = Role('local_user')
-staff = Role('staff')
+logged_role = Role('logged_role')
+staff_role = Role('staff_role')
 other_role = Role('other_role')
 special = Role('special')
 
-local_user.add_parent(everyone)
-staff.add_parents(everyone, local_user)
+logged_role.add_parent(everyone)
+staff_role.add_parents(everyone, logged_role)
 
 anonymous = User(roles=[everyone])
-normal_user = User(roles=[local_user])
-staff_user = User(roles=[staff])
+normal_user = User(roles=[logged_role])
+staff_role_user = User(roles=[staff_role])
 special_user = User(roles=[special])
-many_roles_user = User(roles=[local_user, other_role, everyone])
+many_roles_user = User(roles=[logged_role, other_role, everyone])
 
 current_user = anonymous
 
@@ -38,6 +38,8 @@ def makeapp(with_factory=False, use_white=False):
 
     if use_white:
         app.config['RBAC_USE_WHITE'] = True
+    else:
+        app.config['RBAC_USE_WHITE'] = False
 
     if with_factory:
         rbac = RBAC()
@@ -60,17 +62,32 @@ def makeapp(with_factory=False, use_white=False):
         return Response('Hello')
 
     @app.route('/b', methods=['GET', 'POST'])
-    @rbac.allow(roles=['local_user'], methods=['GET'])
-    @rbac.allow(roles=['staff', 'special'], methods=['POST'])
+    @rbac.allow(roles=['logged_role'], methods=['GET'])
+    @rbac.allow(roles=['staff_role', 'special'], methods=['POST'])
     def b():
         return Response('Hello from /b')
 
     @app.route('/c')
     @rbac.allow(roles=['everyone'], methods=['GET'])
-    @rbac.deny(roles=['local_user'], methods=['GET'], with_children=False)
-    @rbac.allow(roles=['staff'], methods=['GET'])
+    @rbac.deny(roles=['logged_role'], methods=['GET'], with_children=False)
+    @rbac.allow(roles=['staff_role'], methods=['GET'])
     def c():
         return Response('Hello from /c')
+
+    @app.route('/d')
+    @rbac.deny(roles=['everyone'], methods=['GET'])
+    def d():
+        return Response('Hello from /d')
+
+    @app.route('/e')
+    @rbac.deny(roles=['everyone'], methods=['GET'], with_children=True)
+    def e():
+        return Response('Hello from /e')
+
+    @app.route('/f', methods=['POST'])
+    @rbac.deny(roles=['logged_role'], methods=['POST'])
+    def f():
+        return Response('Hello from /f')
 
     return app
 
@@ -85,8 +102,8 @@ class UseWhiteApplicationUnitTests(unittest.TestCase):
     def test_set_user_loader(self):
         global current_user
         self.assertEqual(self.rbac._user_loader(), current_user)
-        self.rbac.set_user_loader(lambda: staff_user)
-        self.assertEqual(self.rbac._user_loader(), staff_user)
+        self.rbac.set_user_loader(lambda: staff_role_user)
+        self.assertEqual(self.rbac._user_loader(), staff_role_user)
         # Restore
         self.rbac.set_user_loader(lambda: current_user)
 
@@ -94,12 +111,15 @@ class UseWhiteApplicationUnitTests(unittest.TestCase):
         global current_user
         current_user = anonymous
         self.assertEqual(self.client.open('/').data, 'index')
+
         current_user = normal_user
         self.assertEqual(self.client.open('/').data, 'index')
         self.assertEqual(self.client.open('/b').data, 'Hello from /b')
-        current_user = staff_user
+
+        current_user = staff_role_user
         self.assertEqual(self.client.open('/').data, 'index')
         self.assertEqual(self.client.open('/b').data, 'Hello from /b')
+
         current_user = special_user
         self.assertEqual(self.client.open('/a').data, 'Hello')
 
@@ -108,17 +128,21 @@ class UseWhiteApplicationUnitTests(unittest.TestCase):
         current_user = special_user
         self.assertEqual(self.client.open('/').status_code, 403)
         self.assertEqual(self.client.open('/b').status_code, 403)
+
         current_user = anonymous
         self.assertEqual(self.client.open('/a').status_code, 403)
+
         current_user = normal_user
         self.assertEqual(self.client.open('/a').status_code, 403)
-        current_user = staff_user
+
+        current_user = staff_role_user
         self.assertEqual(self.client.open('/a').status_code, 403)
 
     def test_allow_post_view(self):
         global current_user
-        current_user = staff_user
+        current_user = staff_role_user
         self.assertEqual(self.client.post('/b').data, 'Hello from /b')
+
         current_user = special_user
         self.assertEqual(self.client.post('/b').data, 'Hello from /b')
 
@@ -126,6 +150,7 @@ class UseWhiteApplicationUnitTests(unittest.TestCase):
         global current_user
         current_user = anonymous
         self.assertEqual(self.client.post('/b').status_code, 403)
+
         current_user = normal_user
         self.assertEqual(self.client.post('/b').status_code, 403)
 
@@ -133,9 +158,11 @@ class UseWhiteApplicationUnitTests(unittest.TestCase):
         global current_user
         current_user = anonymous
         self.assertEqual(self.client.open('/c').data, 'Hello from /c')
+
         current_user = normal_user
         self.assertEqual(self.client.open('/c').status_code, 403)
-        current_user = staff_user
+
+        current_user = staff_role_user
         self.assertEqual(self.client.open('/c').data, 'Hello from /c')
 
     def test_hook(self):
@@ -153,7 +180,7 @@ class UseWhiteApplicationUnitTests(unittest.TestCase):
         self.assertTrue(self.rbac.has_permission('GET', 'c'))
         self.assertFalse(self.rbac.has_permission('GET', 'a'))
         self.assertFalse(self.rbac.has_permission('POST', 'index'))
-        
+
         current_user = special_user
         self.assertTrue(self.rbac.has_permission('GET', 'a'))
         self.assertTrue(self.rbac.has_permission('POST', 'b'))
@@ -163,18 +190,65 @@ class UseWhiteApplicationUnitTests(unittest.TestCase):
         self.assertTrue(self.rbac.has_permission('POST', 'b', special_user))
 
 
+class NoWhiteApplicationUnitTests(unittest.TestCase):
+
+    def setUp(self):
+        self.app = makeapp(use_white=False)
+        self.client = self.app.test_client()
+        self.rbac = self.app.extensions['rbac'].rbac
+
+    def test_allow_get_view(self):
+        global current_user
+        current_user = normal_user
+        self.assertEqual(self.client.open('/d').data, 'Hello from /d')
+
+        current_user = staff_role_user
+        self.assertEqual(self.client.open('/d').data, 'Hello from /d')
+
+    def test_deny_get_view(self):
+        global current_user
+        current_user = anonymous
+        self.assertEqual(self.client.open('/d').status_code, 403)
+        self.assertEqual(self.client.open('/e').status_code, 403)
+
+        current_user = normal_user
+        self.assertEqual(self.client.open('/e').status_code, 403)
+
+        current_user = staff_role_user
+        self.assertEqual(self.client.open('/e').status_code, 403)
+
+    def test_allow_post_view(self):
+        global current_user
+        current_user = anonymous
+        self.assertEqual(self.client.post('/f').data, 'Hello from /f')
+
+        current_user = staff_role_user
+        self.assertEqual(self.client.post('/f').data, 'Hello from /f')
+
+    def test_deny_post_view(self):
+        global current_user
+        current_user = normal_user
+        self.assertEqual(self.client.post('/f').status_code, 403)
+
+    def test_has_permission(self):
+        global current_user
+        current_user = normal_user
+        self.assertTrue(self.rbac.has_permission('GET', 'd'))
+        self.assertFalse(self.rbac.has_permission('POST', 'f'))
+
+
 class RoleMixInUnitTests(unittest.TestCase):
 
     def test_role_get_name(self):
         self.assertEqual(everyone.get_name(), 'everyone')
-        self.assertEqual(local_user.get_name(), 'local_user')
-        self.assertEqual(staff.get_name(), 'staff')
+        self.assertEqual(logged_role.get_name(), 'logged_role')
+        self.assertEqual(staff_role.get_name(), 'staff_role')
 
     def test_add_parent(self):
         normal_role = Role('normal')
         base_role = Role('base')
         normal_role.add_parent(base_role)
-        
+
         self.assertIn(base_role, normal_role.parents)
 
     def test_add_parents(self):
@@ -182,41 +256,41 @@ class RoleMixInUnitTests(unittest.TestCase):
         parent_role = Role('parent')
         another_parent_role = Role('another_parent')
         normal_role.add_parents(parent_role, another_parent_role)
-        
+
         self.assertIn(parent_role, normal_role.parents)
         self.assertIn(another_parent_role, normal_role.parents)
 
     def test_get_parents(self):
         everyone_parents = set()
-        local_user_parents = set()
-        staff_parents = set()
-        
+        logged_role_parents = set()
+        staff_role_parents = set()
+
         for p in everyone.get_parents():
             everyone_parents.add(p)
-        for p in local_user.get_parents():
-            local_user_parents.add(p)
-        for p in staff.get_parents():
-            staff_parents.add(p)
-        
+        for p in logged_role.get_parents():
+            logged_role_parents.add(p)
+        for p in staff_role.get_parents():
+            staff_role_parents.add(p)
+
         self.assertEqual(everyone_parents, set([]))
-        self.assertEqual(local_user_parents, set([everyone]))
-        self.assertEqual(staff_parents, set([everyone, local_user]))
+        self.assertEqual(logged_role_parents, set([everyone]))
+        self.assertEqual(staff_role_parents, set([everyone, logged_role]))
 
     def test_get_children(self):
         everyone_children = set()
-        local_user_children = set()
-        staff_children = set()
-        
+        logged_role_children = set()
+        staff_role_children = set()
+
         for c in everyone.get_children():
             everyone_children.add(c)
-        for c in local_user.get_children():
-            local_user_children.add(c)
-        for c in staff.get_children():
-            staff_children.add(c)
-        
-        self.assertEqual(everyone_children, set([local_user, staff]))
-        self.assertEqual(local_user_children, set([staff]))
-        self.assertEqual(staff_children, set([]))
+        for c in logged_role.get_children():
+            logged_role_children.add(c)
+        for c in staff_role.get_children():
+            staff_role_children.add(c)
+
+        self.assertEqual(everyone_children, set([logged_role, staff_role]))
+        self.assertEqual(logged_role_children, set([staff_role]))
+        self.assertEqual(staff_role_children, set([]))
 
 
 class UserMixInUnitTests(unittest.TestCase):
@@ -240,16 +314,16 @@ class UserMixInUnitTests(unittest.TestCase):
 
     def test_get_roles(self):
         normal_user_roles = set()
-        staff_user_roles = set()
+        staff_role_user_roles = set()
         many_roles_user_roles = set()
 
         for r in normal_user.get_roles():
             normal_user_roles.add(r)
-        for r in staff_user.get_roles():
-            staff_user_roles.add(r)
+        for r in staff_role_user.get_roles():
+            staff_role_user_roles.add(r)
         for r in many_roles_user.get_roles():
             many_roles_user_roles.add(r)
-        
-        self.assertEqual(normal_user_roles, set([local_user]))
-        self.assertEqual(staff_user_roles, set([staff]))
-        self.assertEqual(many_roles_user_roles, set([everyone, local_user, other_role]))
+
+        self.assertEqual(normal_user_roles, set([logged_role]))
+        self.assertEqual(staff_role_user_roles, set([staff_role]))
+        self.assertEqual(many_roles_user_roles, set([everyone, logged_role, other_role]))
